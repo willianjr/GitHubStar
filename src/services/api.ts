dcd9f1d0a54c1d8c7152d0138508dd472c6eb6f4
willianjr <willianrochajunior@outlook.com>
2021-09-26 22:01:02 -0300

    Alteração Chave

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: 'https://api.github.com/graphql',
-    headers: { Authorization: 'Bearer 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' },
+    headers: { Authorization: 'Bearer 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' },
 })
 
 export default api
