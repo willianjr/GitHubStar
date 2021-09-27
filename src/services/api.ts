fdb4ee948403a859533e96b87c29e099254596f0
willianjr <willianrochajunior@outlook.com>
2021-09-26 22:08:39 -0300

    Chave

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: 'https://api.github.com/graphql',
-    headers: { Authorization: 'Bearer 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' },
+    headers: { Authorization: 'Bearer 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' },
 })
 
 export default api
